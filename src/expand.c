e19d5f9c5e75ddbbeec86b8a8367891e9846e9ab
Rodrigo Carvalho <rcarvalh@c2t1s3.42.rio>
2023-07-13 17:37:17 -0300

    only initial comments

---
+++ src/expand.c
@@ -0,0 +1,11 @@
+// need to export more than 1 variable at the same time??
+
+// ler regras do export (man) 
+
+// ler regras variaveis shell
+
+//unset em variavel inexistente é igual ao comportamento normal, não dá erro
+
+
+
+
