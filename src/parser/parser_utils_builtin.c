80edef068f974fe681764bba1e5c7290d90be94f
rodrigo <rodrigoadcarvalho@gmail.com>
2023-07-16 15:29:19 -0300

    included type of builtins in enum

--- src/parser/parser_utils_builtin.c
+++ src/parser/parser_utils_builtin.c
@@ -10,43 +10,43 @@ int ft_is_builtin(t_token **list)
 		//echo
 		if (ft_strcmp(aux->data, "echo") == 0)
 		{
-			aux->type = BUILTIN;
+			aux->type = BUILTIN_ECHO;
 			printf("token %i is builtin echo\n", aux->index);
 		}
 		//cd
 		if (ft_strcmp(aux->data, "cd") == 0)
 		{
-			aux->type = BUILTIN;
+			aux->type = BUILTIN_CD;
 			printf("token %i is builtin cd\n", aux->index);
 		}
 		//pwd
 		if (ft_strcmp(aux->data, "pwd") == 0)
 		{
-			aux->type = BUILTIN;
+			aux->type = BUILTIN_PWD;
 			printf("token %i is builtin pwd\n", aux->index);
 		}
 		//export
 		if (ft_strcmp(aux->data, "export") == 0)
 		{
-			aux->type = BUILTIN;
+			aux->type = BUILTIN_EXPORT;
 			printf("token %i is builtin export\n", aux->index);
 		}
 		//unset
 		if (ft_strcmp(aux->data, "unset") == 0)
 		{
-			aux->type = BUILTIN;
+			aux->type = BUILTIN_UNSET;
 			printf("token %i is builtin unset\n", aux->index);
 		}
 		//env
 		if (ft_strcmp(aux->data, "env") == 0)
 		{
-			aux->type = BUILTIN;
+			aux->type = BUILTIN_ENV;
 			printf("token %i is builtin env\n", aux->index);
 		}
 		//exit
 		if (ft_strcmp(aux->data, "exit") == 0)
 		{
-			aux->type = BUILTIN;
+			aux->type = BUILTIN_EXIT;
 			printf("token %i is builtin exit\n", aux->index);
 		}
 		aux = aux->next;
