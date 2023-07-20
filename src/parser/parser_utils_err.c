540df7c974178c41e8dbe3e3a510d011c42aeaa7
rodrigo <rodrigoadcarvalho@gmail.com>
2023-07-19 21:22:40 -0300

    included header as solicited

--- src/parser/parser_utils_err.c
+++ src/parser/parser_utils_err.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_utils_err.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcarvalh <rcarvalh@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/19 21:21:17 by rcarvalh          #+#    #+#             */
+/*   Updated: 2023/07/19 21:21:18 by rcarvalh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../inc/minishell.h"
 
 //when string initiates with <> next word is the name of a file to open/create
