a1a92d67890040e2f96adb42d1ac2916e0cc5dab
rodrigo <rodrigoadcarvalho@gmail.com>
2023-07-27 05:52:45 -0300

    UPDATED TODO

--- src/parser/parser_join_cmds.c
+++ src/parser/parser_join_cmds.c
@@ -6,7 +6,7 @@
 /*   By: rcarvalh <rcarvalh@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 17:29:17 by rcarvalh          #+#    #+#             */
-/*   Updated: 2023/07/27 05:24:16 by rcarvalh         ###   ########.fr       */
+/*   Updated: 2023/07/27 05:47:38 by rcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,61 +106,6 @@ void	ft_print_cmds(t_token *list)
 }
 
 //ft_print_check usada para verificar se os tokens estavam sendo marcados direito
-//TODO
-// verificar se o token de builtin está sendo marcado corretamente (IMPORTANTE!!!)
-// verificar se o token de erro está sendo marcado corretamente - FIXED in parser_utils_redirs.c
-// verificar se o token de argumento está sendo marcado corretamente - nao esta sendo marcado
-/*
-aqui acho que é preciso uma função com a lógica do bash
-- se for builtin, verifica qual é e marca (chamar a função ft_is_bultin aqui???)
-tô achando que vamos acabar trabalhando com a lista, modificando ela até o final.
-
-
-TODO
-- depois de pipe vem um argumento, é um comando?
-ou pode ser um redirecionamento? ou um pipe? ou uma builtin?
-
-AQUI preciso de ajuda pra entender a lógica do bash e ir codando comigo algumas funções
-- depois de uma builtin é um argumento? nem sempre, né? e se for um pipe? ou um redirecionamento?
-quais funcionam e não funcionam? 
-
-até então só estão excluídos os casos:
-// inicia com pipe;
-// termina com pipe vazio;
-// termina com pipe depois de um redirecionamento - checar essa lógica
-// termina com redirecionamento depois de um pipe - checar essa lógica
-// pipe depois de pipe, redirecionamento depois de redirecionamento (e variações)
-será que peguei todas?
-
-- depois de um redirecionamento é um argumento?
-tem que abrir arquivo? tem que armazenar o fd do arquivo? fazer fd_in e fd_out no token?
-- depois de um pipe, se não for builtin, é um comando?
-verificar se pode ter outras possibilidades
-
-
-- redirecionamento depois de pipe? Existe? Funciona? Quais? Como? 
-
-- pipe depois de redirecionamento? Existe? Funciona? Quais? Como?
-
-- redirecionamento depois de builtin? Existe? Funciona? Quais? Como?
-
-- builtin depois de redirecionamento? Existe? Funciona? Quais? Como?
-
-- builtin depois de pipe? Existe? Funciona? Quais? Como?
-
-- pipe depois de builtin? Existe? Funciona? Quais? Como?
-
-- builtin depois de builtin;
-
-*/
-   
-// verificar se o token de expand está sendo marcado corretamente - strings expandidas já são devolvidas nos tokens token->str;
-// verificar se o token de pipe está sendo marcado corretamente - OK - eu acho
-// verificar se o token de heredoc está sendo marcado corretamente - OK
-// verificar se o token de redirecionamento está sendo marcado corretamente - OK
-// verificar se o token de comando está sendo marcado corretamente - marcado agora, na função ft_parse_cmds
-
-
 
 void	ft_print_check(t_shell *shell)
 {
