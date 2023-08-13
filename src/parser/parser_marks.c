fb8ce0f7730da78624bfb7fc3e4ec7b60c96b4f9
rodrigo <rodrigoadcarvalho@gmail.com>
2023-08-12 23:31:44 -0300

    header and norminette

--- src/parser/parser_marks.c
+++ src/parser/parser_marks.c
@@ -6,7 +6,7 @@
 /*   By: rcarvalh <rcarvalh@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 18:54:29 by rcarvalh          #+#    #+#             */
-/*   Updated: 2023/08/12 18:55:03 by rcarvalh         ###   ########.fr       */
+/*   Updated: 2023/08/12 22:21:19 by rcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,5 @@ int	ft_marks(t_shell *shell)
 	ft_mark_append_file(shell);
 	ft_mark_cmds(shell);
 	ft_mark_args(shell);
-
 	return (0);
 }
