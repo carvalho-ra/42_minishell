dbc24d04be9950a5276337e25f7761d82640f577
rodrigo <rodrigoadcarvalho@gmail.com>
2023-08-20 11:00:01 -0300

    still trying

--- src/lexer/lexer_utils_list.c
+++ src/lexer/lexer_utils_list.c
@@ -6,7 +6,7 @@
 /*   By: rcarvalh <rcarvalh@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 19:30:04 by rcarvalh          #+#    #+#             */
-/*   Updated: 2023/08/20 10:23:34 by rcarvalh         ###   ########.fr       */
+/*   Updated: 2023/08/20 10:57:43 by rcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,10 @@ t_token	*ft_create_node(char *str, int index, t_shell *shell)
 	new_node->next = NULL;
 	new_node->fd_in = STDIN_FILENO;
 	new_node->fd_out = STDOUT_FILENO;
-	new_node->pipe[0] = STDIN_FILENO;
-	new_node->pipe[1] = STDOUT_FILENO;
-	new_node->backup[0] = STDIN_FILENO;
-	new_node->backup[1] = STDOUT_FILENO;
+	new_node->pipe[0] = 0;
+	new_node->pipe[1] = 1;
+	new_node->backup[0] = 0;
+	new_node->backup[1] = 1;
 	return (new_node);
 }
 
