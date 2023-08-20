ecea40aad6c109201185a3e26d730d85b3807ac5
rodrigo <rodrigoadcarvalho@gmail.com>
2023-08-20 11:07:21 -0300

    FUCK!

--- src/exec/execution.c
+++ src/exec/execution.c
@@ -6,7 +6,7 @@
 /*   By: rcarvalh <rcarvalh@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 21:21:12 by rcarvalh          #+#    #+#             */
-/*   Updated: 2023/08/20 10:38:02 by rcarvalh         ###   ########.fr       */
+/*   Updated: 2023/08/20 11:05:15 by rcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,7 +89,8 @@ int	ft_execution(t_shell *shell)
 			}
 			ft_reset_fds(token);
 		}
-		else if (token->type >= REDIRECT_IN && token->type <= APPEND)
+		else if (token->type == REDIRECT_IN || token->type == REDIRECT_OUT
+			|| token->type == APPEND)
 			ft_reset_fds(token);
 		token = token->next;
 	}
